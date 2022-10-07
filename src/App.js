b2fd0a6c67ccef455f6a3636064ce3d7ae86bb00
Ahn Shields <ahnshields@gmail.com>
2022-10-07 12:27:59 -0700

    Configure two Routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import FooterComponent from './components/FooterComponent';
 import HeaderComponent from './components/HeaderComponent';
 import { ListEmployeeComponent } from './components/ListEmployeeComponent';
@@ -7,9 +7,17 @@ import { ListEmployeeComponent } from './components/ListEmployeeComponent';
 function App() {
   return (
     <div>
-      <HeaderComponent />
-      <ListEmployeeComponent />  
-      <FooterComponent />    
+      <Router>    
+        <HeaderComponent />
+        <div className= "container">
+          <Routes>            
+            <Route exact path = "/" element = { <ListEmployeeComponent />}></Route>
+            <Route path = "/employees" element = { <ListEmployeeComponent />}></Route>
+          </Routes>
+        {/* <ListEmployeeComponent />  -- don't need it because Routes are added for it         */}
+        </div>
+        <FooterComponent /> 
+      </Router>   
     </div>
   );
 }

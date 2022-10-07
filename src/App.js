f42d2f60281ba54257d9fe10488204b4eccda928
Ahn Shields <ahnshields@gmail.com>
2022-10-07 09:22:59 -0700

    add work

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { ListEmployeeComponent } from './components/ListEmployeeComponent';
+
+function App() {
+  return (
+    <div>
+      <ListEmployeeComponent />      
+    </div>
+  );
+}
+
+export default App;

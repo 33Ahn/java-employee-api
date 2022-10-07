6c6cc226e40ab67e4a58028d9882063a6a0605b2
Ahn Shields <ahnshields@gmail.com>
2022-10-07 11:27:16 -0700

    Add Header and Footer Components to React Application

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function FooterComponent() {
+  return (
+    <div>
+        <footer className = "footer">
+            <span className = "text-muted">All Right Reserved 2022 AhnShields</span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent

b8b72f7bb2d84ef7410996e8dce4ec8b538193d5
Ahn Shields <ahnshields@gmail.com>
2022-10-07 13:39:04 -0700

    Create AddEmployeeComponent.js, add <Link> in ListEmployeeComponent.js, create a Route for '/add-employee' in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import FooterComponent from './components/FooterComponent';
-import HeaderComponent from './components/HeaderComponent';
-import { ListEmployeeComponent } from './components/ListEmployeeComponent';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import FooterComponent from "./components/FooterComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import { ListEmployeeComponent } from "./components/ListEmployeeComponent";
+import AddEmployeeComponent  from "./components/AddEmployeeComponent";
 
 function App() {
   return (
     <div>
-      <Router>    
+      <Router>
         <HeaderComponent />
-        <div className= "container">
-          <Routes>            
-            <Route exact path = "/" element = { <ListEmployeeComponent />}></Route>
-            <Route path = "/employees" element = { <ListEmployeeComponent />}></Route>
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<ListEmployeeComponent />}></Route>
+            <Route
+              path="/employees"
+              element={<ListEmployeeComponent />}
+            ></Route>
+            <Route
+              path="/add-employee"
+              element={<AddEmployeeComponent />}
+            ></Route>
           </Routes>
-        {/* <ListEmployeeComponent />  -- don't need it because Routes are added for it         */}
         </div>
-        <FooterComponent /> 
-      </Router>   
+        <FooterComponent />
+      </Router>
     </div>
   );
 }
